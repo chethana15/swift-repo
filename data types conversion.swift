230ee8ef07d18b04258db612c786909b43b9218b
chethana15 <90371115+chethana15@users.noreply.github.com>
2021-09-27 13:07:16 +0530

    Add files via upload

---
+++ data types conversion.swift
@@ -0,0 +1,20 @@
+import UIKit
+
+var str = "Hello, playground"
+
+var str1: String = "LET US LEARN SOME DATA TYPES"
+
+print(str1)
+
+var a: Float = 999.89
+var b: Double = 999.892738732
+var c: Bool = true
+
+print(Int(a))
+print(Int(b))
+print(Float(b))
+print(Int(round(b)))
+print(Double(a))
+
+
+
