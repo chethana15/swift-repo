167fe6190ecd3cba98e5226be5bfa739ea3ce2e7
Chethana N P <90371115+chethana15@users.noreply.github.com>
2021-09-27 16:45:53 +0530

    Add files via upload

---
+++ multi_line string.swift
@@ -0,0 +1,15 @@
+import UIKit
+
+//var str = "Hello, playground"
+var str =  """
+this helps
+to create multi line
+string
+"""
+var str_b = """
+I'm learning \
+swift \
+programming
+"""
+print(str)
+print(str_b)
