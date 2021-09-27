5780a0825f9a44beba0beef6fa605359817e68e4
chethana15 <90371115+chethana15@users.noreply.github.com>
2021-09-27 12:37:42 +0530

    Add files via upload

---
+++ basic arithmetic operation.swift
@@ -0,0 +1,24 @@
+import UIKit
+
+var str = "Hello, playground"
+print(str)
+
+var a = 1
+var b = 1
+
+a=2
+
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+
+let c = 10
+let d = 11
+
+print(c + d)
+print(c - d)
+print(c * d)
+print(c / d)
+
+
