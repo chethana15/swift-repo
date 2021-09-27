ba352ba5b395e292b81cbef77b85b26c9c53b545
chethana15 <90371115+chethana15@users.noreply.github.com>
2021-09-27 10:12:32 +0530

    Add files via upload

---
+++ hello swift.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  hello world
+//
+//  Created by Cumulations Technologies on 27/09/21.
+//  Copyright © 2021 Chethana. All rights reserved.
+//
+
+var myFirstString = "Hello, Swift!"
+
+print("myFirstString")
+
