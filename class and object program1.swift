48a4e44e5826d0924f9a505d568d969ed4db42d9
Chethana N P <90371115+chethana15@users.noreply.github.com>
2021-09-28 17:42:14 +0530

    Create class and object program1.swift

---
+++ class and object program1.swift
@@ -0,0 +1,44 @@
+import UIKit
+
+class FRIENDS_Series{
+    var friendName = "";
+    var celebrityOfFriends = "";
+}
+
+var friend1 = FRIENDS_Series()
+
+friend1.friendName = "CHARAN"
+friend1.celebrityOfFriends = "ROSS"
+
+var friend2 = FRIENDS_Series()
+
+friend2.friendName = "NAVEEN"
+friend2.celebrityOfFriends = "CHANDLER"
+
+var friend3 = FRIENDS_Series()
+
+friend3.friendName = "SUMANTH"
+friend3.celebrityOfFriends = "JOEY"
+
+var friend4 = FRIENDS_Series()
+
+friend4.friendName = "PAVITHRA"
+friend4.celebrityOfFriends = "MONICA"
+
+var friend5 = FRIENDS_Series()
+
+friend5.friendName = "PALLAVI"
+friend5.celebrityOfFriends = "PHOEBE"
+
+var friend6 = FRIENDS_Series()
+
+friend6.friendName = "DEEKSHA"
+friend6.celebrityOfFriends = "RACHEL"
+
+print("\(friend1.friendName) likes \(friend1.celebrityOfFriends)")
+print("\(friend2.friendName) likes \(friend2.celebrityOfFriends)")
+print("\(friend3.friendName) likes \(friend3.celebrityOfFriends)")
+print("\(friend4.friendName) likes \(friend4.celebrityOfFriends)")
+print("\(friend5.friendName) likes \(friend5.celebrityOfFriends)")
+print("\(friend6.friendName) likes \(friend6.celebrityOfFriends)")
+ 
