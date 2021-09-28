8ad9cea6ba9a164f30ada587ee2a28b9feb6c66b
Chethana N P <90371115+chethana15@users.noreply.github.com>
2021-09-28 09:28:02 +0530

    Create tuples.swift

---
+++ tuples.swift
@@ -0,0 +1,17 @@
+import UIKit
+
+var nameRegisterNumber = ("Chethana", 7013);
+
+print("name :", nameRegisterNumber.0);
+
+print("register number :", nameRegisterNumber.1);
+
+//to modify tuple
+
+nameRegisterNumber.0 = "Chethu"
+nameRegisterNumber.1 = 7014
+
+print("Modified tuple =", nameRegisterNumber.0)
+print("Modified tuple =", nameRegisterNumber.1)
+
+//we cannot add or remove elements for a tuple in swift
