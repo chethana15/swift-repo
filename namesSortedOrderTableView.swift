fad5c591a82b90dcb5e7db68e4ea1ddcf42a215a
Chethana N P <90371115+chethana15@users.noreply.github.com>
2021-10-08 18:04:12 +0530

    Create namesSortedOrderTableView.swift

---
+++ namesSortedOrderTableView.swift
@@ -0,0 +1,39 @@
+import UIKit
+
+class ViewController: UIViewController ,UITableViewDelegate,UITableViewDataSource{
+    
+    
+
+    let myTableView = UITableView()
+     
+    var friendsNames = ["BRUNDA", "NIRIKSHA", "CHETHANA", "POOJA", "JAYSHEELA", "PARVATHI"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+        view.addSubview(myTableView)
+        friendsNames.sort(by: <)
+        myTableView.register( UITableViewCell.self, forCellReuseIdentifier: "names")
+        myTableView.delegate = self
+        myTableView.dataSource = self
+    }
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        myTableView.frame = view.bounds
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return friendsNames.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let names = myTableView.dequeueReusableCell(withIdentifier: "names", for: indexPath)
+        names.textLabel?.text = friendsNames[indexPath.item]
+        return names
+    }
+    /*func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    }*/
+}
+
+
+
